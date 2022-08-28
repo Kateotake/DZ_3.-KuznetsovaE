05eaf8a745f6885298ec388fed949136b9fc62ab
KuznetsovaE <kettt56@mail.ru>
2022-08-29 02:34:34 +0300

    DZ3

---
+++ Zadacha_19/Zadacha_19.cs
@@ -0,0 +1,22 @@
+﻿// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+ Console.Write ("введите пятизначное число N: ");
+    string N = Console.ReadLine();
+    int dlina = N.Length;
+    int K = Convert.ToInt32 (N);
+    int n1 = K/10000;
+    int n2 = (K/1000) % 10;
+    int n4 = (K/10) % 10;
+    int n5 = K % 10;
+
+if (dlina == 5)
+{
+    if ((n1 == n5) && (n2 == n4)) Console.WriteLine("Да");
+    else Console.WriteLine ("Нет");
+}
+else Console.WriteLine ("Нужно пятизначное число");
---
+++ Zadacha_21/Zadacha_21.cs
@@ -0,0 +1,21 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+    Console.Write ("введите координату x Точки А ");
+    double x1 = Convert.ToDouble (Console.ReadLine());
+    Console.Write ("введите координату y Точки А ");
+    double y1 = Convert.ToDouble (Console.ReadLine());
+    Console.Write ("введите координату z Точки А ");
+    double z1 = Convert.ToDouble (Console.ReadLine());
+    Console.Write ("введите координату x Точки B ");
+    double x2 = Convert.ToDouble (Console.ReadLine());
+    Console.Write ("введите координату y Точки В ");
+    double y2 = Convert.ToDouble (Console.ReadLine());
+    Console.Write ("введите координату z Точки B ");
+    double z2 = Convert.ToDouble (Console.ReadLine());
+
+    double res = Math.Sqrt (Math.Pow((x1 - x2),2) + Math.Pow((y1 - y2),2) + Math.Pow((z1 - z2),2));
+
+    Console.WriteLine(res);
